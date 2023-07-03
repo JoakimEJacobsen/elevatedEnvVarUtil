99c8e7caa850ec708bc4924ad811c80e6eddd62c
Joakim E Jacobsen <joakim.jacobsen@nav.no>
2023-07-03 14:48:14 +0200

    added support for openjdk 20

--- changeJavaEnvVar.py
+++ changeJavaEnvVar.py
@@ -1,7 +1,7 @@
 import sys, os, subprocess
 
-versionPathMap = {'8':os.environ['JAVA8'],'11':os.environ['JAVA11'],'16':os.environ['JAVA16'],'17':os.environ['JAVA17']}
-validArguments = ['8','11','16','17']
+versionPathMap = {'8':os.environ['JAVA8'],'11':os.environ['JAVA11'],'16':os.environ['JAVA16'],'17':os.environ['JAVA17'],'20':os.environ['JAVA20']}
+validArguments = ['8','11','16','17','20']
 firstArgument = sys.argv[1]
 print("First argument passed to script is: " + firstArgument)
 
@@ -15,7 +15,7 @@ def changeJavaVersion(_firstArgument):
     else:
         print("""
         This script changes the JAVA_HOME system environment variable.
-        Available input options are 8, 11, 16 (or x to exit):
+        Available input options are 8, 11, 16, 17, 20 (or x to exit):
         """)
         userInput = input("Enter input:")
         changeJavaVersion(userInput)
